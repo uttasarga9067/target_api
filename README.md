c37ae825717bbb8209958a2e32df8db5bdaeaa74
Uttasarga Singh <uttasargasingh9067@gmail.com>
2021-08-10 22:19:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+PRODUCT REGISTRY SERVICE
+
+This particular Application gets a product ID as a GET Request, in order to retrieve the important fields about that product.
+
+The Data is in the following format:
+[{"product":
+    {"available_to_promise_network":
+        {"product_id":"13860428",
+        "id_type":"TCIN",
+        "available_to_promise_quantity":10.0,
+        "street_date":"2011-11-15T06:00:00.000Z",
+        "availability":"AVAILABLE",
+        "online_available_to_promise_quantity":10.0,
+        "stores_available_to_promise_quantity":0.0,
+        "availability_status":"IN_STOCK",
+        "multichannel_options":[],
+        "is_infinite_inventory":false,
+        "loyalty_availability_status":"IN_STOCK",
+        "loyalty_purchase_start_date_time":"1970-01-01T00:00:00.000Z",
+        "is_loyalty_purchase_enabled":false,
+        "is_out_of_stock_in_all_store_locations":false,
+        "is_out_of_stock_in_all_online_locations":false},
+            "item":
+                {"tcin":"13860428",
+                "bundle_components":{},
+                "dpci":"058-34-0436",
+                "upc":"025192110306",
+                "product_description":
+                {"title":"The Big Lebowski (Blu-ray)"}]
+
---
+++ product_registry/__init__.py
@@ -0,0 +1,82 @@
+import markdown
+import os
+import json
+import requests
+from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+import sqlalchemy
+# Import the framework
+
+
+from flask import Flask, g,jsonify
+from flask_restful import Resource, Api, reqparse, request
+
+
+# Create an instance of Flask
+app = Flask(__name__)
+# Create the API
+api = Api(app)
+
+@app.route('/processjson', methods=['GET'])
+
+# """THIS GET METHOD IS JUST A DEMO FOR MAKING SURE THAT I CAN PROCESS THE JSON AS PER MY REQUIREMENT"""
+
+# """UTTASARGA SINGH 08/08/2021"""
+def processjson():
+    uri = "https://redsky.target.com/v3/pdp/tcin/13860428?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics&key=candidate#_blank"
+    try:
+        uResponse = requests.get(uri)
+    except requests.ConnectionError:
+        return "Connection Error"
+    Jresponse = uResponse.text
+    data = json.loads(Jresponse)
+    product_id = data['product']['available_to_promise_network']['product_id']
+    name = data['product']['item']['product_description']['title']
+    # reputation = data['items'][0]['reputation']
+    # return Jresponse
+    return json.dumps({'ID': product_id, 'NAME': name})
+
+
+    # name = json.dumps(data[0])
+    # return json.dumps({'result': 'Success', 'displayName': displayName})
+@app.route("/")
+def index():
+    # """PRESENT THE CODE DOCUMENTATION"""
+
+    # """UTTASARGA SINGH 08/08/2021"""
+
+    # Open the README file
+    with open(os.path.dirname(app.root_path) + '/README.md', 'r') as markdown_file:
+
+        # Read the content of the file
+        content = markdown_file.read()
+
+        # Convert to HTML
+        return markdown.markdown(content)
+
+class Device(Resource):
+
+    # """THIS METHOD GETS A PRODUCT ID AS A INPUT FROM THE USER AND HELPS TO RETRIEVE THE DATA POINTS WITH REGARDS TO THE DATA"""
+
+    # """UTTASARGA SINGH 08/08/2021"""
+    def get(self, identifier):
+        uri = "https://redsky.target.com/v3/pdp/tcin/13860428?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics&key=candidate#_blank"
+        try:
+            uResponse = requests.get(uri)
+        except requests.ConnectionError:
+            return "Connection Error"
+        Jresponse = uResponse.text
+        data = json.loads(Jresponse)
+        # If the key does not exist in the data store, return a 404 error.
+        if identifier not in data['product']['available_to_promise_network']['product_id']:
+            return {'message': 'Product not found', 'data': {}}, 404
+        else:
+            return {'message': 'PRODUCT FOUND',
+                    'PRODUCT_ID': data['product']['available_to_promise_network']['product_id'],
+                    'PRODUCT_NAME': data['product']['item']['product_description']['title']}, 200
+
+
+# api.add_resource(DeviceList, '/devices')
+api.add_resource(Device, '/device/<string:identifier>')
